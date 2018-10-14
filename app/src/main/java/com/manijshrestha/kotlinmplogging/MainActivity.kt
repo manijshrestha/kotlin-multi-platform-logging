597ba331c9bbff828515bf30f531135e5aaca7e4
Manij Shrestha <manijshrestha@gmail.com>
2018-10-14 11:21:18 -0500

    Implemented analytics events

--- app/src/main/java/com/manijshrestha/kotlinmplogging/MainActivity.kt
+++ app/src/main/java/com/manijshrestha/kotlinmplogging/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.manijshrestha.kotlinmplogging
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.manijshrestha.kotlinmplogging.analytics.ButtonClickEvent
+import com.manijshrestha.kotlinmplogging.analytics.ViewEvent
 
 class MainActivity : AppCompatActivity() {
+
+    private val analyticsManager = AndroidAnatlyicsManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        analyticsManager.report(ViewEvent("Main Page"))
+    }
+
+    fun onButtonClick(view: View) {
+        when (view.id) {
+            R.id.red_button -> analyticsManager.report(ButtonClickEvent("Red"))
+            R.id.green_button -> analyticsManager.report(ButtonClickEvent("Green"))
+            R.id.blue_button -> analyticsManager.report(ButtonClickEvent("Blue"))
+        }
     }
 }
