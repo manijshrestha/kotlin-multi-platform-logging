12cde8f2121725d0a2991b6d096aa4cd79b1211d
Manij Shrestha <manijshrestha@gmail.com>
2018-10-14 11:40:33 -0500

    Added iOS App

---
+++ iosApp/KotlinMPLogging/KotlinMPLogging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KotlinMPLogging
+//
+//  Created by Manij Shrestha on 10/14/18.
+//  Copyright © 2018 Manij Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
